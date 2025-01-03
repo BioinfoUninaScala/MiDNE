d0678e666a0137ba03a5a84896e127a7ed3d7393
aurora brandi <aurora@hypatia>
2025-01-03 18:41:34 +0100

    intersection

--- R/get_intersection_matrices.R
+++ R/get_intersection_matrices.R
@@ -1,28 +1,10 @@
-#' Create transition probabilities matrix
+#' Get intersection between omics matrices
 #'
-#' @importFrom dplyr inner_join
-#' @param omics_list
-#' a named list of data frames containing an omics multiplex network.
-#' Each data frame in the list is required to have at least two columns containing nodes ids respectively named source and destination
-#' and one column named weight containing edge weights.
-#' @return A squared n x n matrix (where n is the number of omics layers) containing  transition probabilities between omics layers.
+#' @param omics_list a list of matrices with features on columns
+#' @return a list of matrices sharing the same column ids
 #' @export
 
-create_layer_transition_matrix <- function(omics_list) {
-  
-  # @param multiplex A multiplex, an object that comprises different omics.
-  
-  #layers <- unique(multiplex$EdgeType)
-  layers <- names(omics_list)
-  jump_mat <- matrix(0, nrow = length(layers), ncol = length(layers), dimnames = list(layers,layers))
-  
-  comb <- utils::combn(layers,2)
-  for (i in 1:ncol(comb))
-    jump_mat[ comb[1,i],comb[2,i] ] <- max(nrow(dplyr::inner_join(omics_list[[ comb[1,i] ]], omics_list[[ comb[2,i] ]], by = c("source","dest"))), 1) /
-    (nrow(omics_list[[ comb[1,i] ]]) + nrow(omics_list[[ comb[2,i] ]]))
-  
-  jump_mat[lower.tri(jump_mat)] <- t(jump_mat)[lower.tri(jump_mat)]
-  
-  jump_mat <- jump_mat/base::rowSums(jump_mat)
-  jump_mat
+get_intersection_matrices <- function(omics_list) {
+  col_inter <- base::Reduce(intersect, base::lapply(omics_list, colnames))
+  base::lapply(omics_list, function(x) x[,col_inter])
 }
