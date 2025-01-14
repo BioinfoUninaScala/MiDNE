354c29f2ccfe5badf4e3510fef47ec88f2135f9e
aurora brandi <aurora@hypatia>
2025-01-14 16:18:11 +0100

    bipartite net

--- R/loadBipartiteNet.R
+++ R/loadBipartiteNet.R
@@ -1,14 +1,13 @@
 #' Load gene-drug network
 #'
 #' @description This function loads gene-drug network included in the `MiDNE` package.
-#' @import readr
 #' @return A dataframe with three columns reporting genes as source nodes, drugs as target nodes and the weights of the edges.
 #' @export
 
 loadBipartiteNet <- function() {
   
   file_path <- system.file("extdata", "networks/bipartite/FDA_active_DRUGBANK_bnet.RDS", package = "MiDNE")
-  file <- readr::read_delim(file = file_path, delim = ',')
+  file <- readRDS(file = file_path)
 
   return(file)
 }
