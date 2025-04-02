2be5c38344007bd26899b5ee723bf8306cde0896
aurora brandi <aurora@hypatia>
2025-04-02 20:11:57 +0200

    commit

--- R/fisher_test_post_hoc.R
+++ R/fisher_test_post_hoc.R
@@ -77,7 +77,7 @@ post_hoc_analysis_2 <- function(cont_table, correction_method){
   
   if (any(diff_obs_exp[-1] > 0)) {
     chisq <- chisq.test(cont_table)
-    res <- chisq$statistic
+    res <- unname(chisq$statistic)
   } else {
     stat <- 0
   }
